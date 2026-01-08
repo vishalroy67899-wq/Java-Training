35b912182335305a88e2118a422ec0c533cd183a
vishalroy67899-wq <vishalroy67899@gmail.com>
2026-01-08 19:20:41 +0530

    Update BankAccount.java12
    ,br>

--- BankAccount.java
+++ BankAccount.java
@@ -53,6 +53,6 @@ class BankAccount {
         BankAccount acc2 = new BankAccount(2002, "Vishal Roy", 5000);
         acc2.deposit(1500);
         acc2.withdraw(2000);
-        acc2.displayDetails();
+        acc2.displayDetails();<br>
     }
 }
